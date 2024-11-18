87185143ac6c9c9d5950fb66990deedef7bb1f1f
edom18 <edo.m18@gmail.com>
2024-11-18 16:06:45 +0900

    format: 型を明示

--- Assets/Project/Scripts/Rendering/NegativeRendererFeature.cs
+++ Assets/Project/Scripts/Rendering/NegativeRendererFeature.cs
@@ -6,21 +6,7 @@ using UnityEngine.Rendering.Universal;
 
 public class NegativeRenderPass : ScriptableRenderPass
 {
-    // private const string ShaderPath = "Hidden/Sample/Negative";
     private Material _material;
-
-    // private Material Material
-    // {
-    //     get
-    //     {
-    //         if (_material == null)
-    //         {
-    //             _material = CoreUtils.CreateEngineMaterial(ShaderPath);
-    //         }
-    //
-    //         return _material;
-    //     }
-    // }
 
     public NegativeRenderPass(Material material)
     {
@@ -42,57 +28,54 @@ public class NegativeRenderPass : ScriptableRenderPass
 
     public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
     {
-        // 解説 *1
-        // frameDataからURP内蔵のリソースデータを取得
-        var resourceData = frameData.Get<UniversalResourceData>();
+        // コンテナから必要なリソースを取得する
+        
+        UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
 
-        // 解説 *2
         // 入力とするテクスチャをresourceDataから取得
-        var sourceTextureHandle = resourceData.activeColorTexture; // activeColorTextureはカメラが描画したメインのカラーバッファ
+        // activeColorTextureはカメラが描画したメインのカラーバッファ
+        
+        TextureHandle sourceTextureHandle = resourceData.activeColorTexture;
 
-        // 解説 *3
         // 出力用のテクスチャのDescriptorを作成
-        var negativeDescriptor = renderGraph.GetTextureDesc(sourceTextureHandle); // 入力テクスチャのDescriptorをコピー
+        
+        TextureDesc negativeDescriptor = renderGraph.GetTextureDesc(sourceTextureHandle);
         negativeDescriptor.name = "NegativeTexture"; // テクスチャの名前を設定
         negativeDescriptor.clearBuffer = false; // クリア不要
         negativeDescriptor.msaaSamples = MSAASamples.None; // MSAA不要
         negativeDescriptor.depthBufferBits = 0; // 深度バッファ不要
 
-        // 解説 *4
         // Descriptorを用いて色反転テクスチャを作成
-        var negativeTextureHandle = renderGraph.CreateTexture(negativeDescriptor);
+        
+        TextureHandle negativeTextureHandle = renderGraph.CreateTexture(negativeDescriptor);
 
-        // 解説 *5
         // カメラカラーを反転し、出力用のテクスチャに描画するRasterRenderPassを作成し、RenderGraphに追加
-        using (var builder = renderGraph.AddRasterRenderPass<PassData>("NegativeRenderPass", out var passData))
+        
+        using (IRasterRenderGraphBuilder builder = renderGraph.AddRasterRenderPass<PassData>("NegativeRenderPass", out PassData passData))
         {
-            // passDataに必要なデータを入れる
             passData.Material = _material;
             passData.SourceTexture = sourceTextureHandle;
 
-            // 解説 *6
             // builderを通してRenderGraphPassに対して各種設定を行う
             // なお、描画ターゲットや他使用されるテクスチャは必ずこの段階で設定する必要がある
             builder.SetRenderAttachment(negativeTextureHandle, 0, AccessFlags.Write); // 描画ターゲットに出力用のテクスチャを設定
             builder.UseTexture(sourceTextureHandle, AccessFlags.Read); // 入力テクスチャの使用を宣言する
 
-            // 解説 *7
             // 実際の描画関数を設定する(static関数が推奨されてる)
+            
             builder.SetRenderFunc(static (PassData data, RasterGraphContext context) =>
             {
-                // 解説 *8
-                // contextからCommandBufferを取得
-                var cmd = context.cmd;
-                var material = data.Material;
-                var source = data.SourceTexture;
-                // Blitterの便利関数を使ってBlit実行
+                RasterCommandBuffer cmd = context.cmd;
+                Material material = data.Material;
+                TextureHandle source = data.SourceTexture;
+                
                 Blitter.BlitTexture(cmd, source, Vector2.one, material, 0);
             });
         }
 
-        // 解説 *9
         // 色反転テクスチャをカメラカラーにBlitする
         // 単純なBlitなら、RenderGraphは便利な関数を提供しているので、それを使う
+        
         renderGraph.AddBlitPass(negativeTextureHandle, sourceTextureHandle, Vector2.one, Vector2.zero, passName: "BlitNegativeTextureToCameraColor");
     }
 }
